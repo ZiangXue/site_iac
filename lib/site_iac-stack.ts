b77c23b4f594db44d201c44a7d829245f0cdc880
ZiangXue <ziangxue@outlook.com>
2026-08-11 15:23:56 -0400

    Initial commit - set up sample stack and env-based stack instantiation

---
+++ lib/site_iac-stack.ts
@@ -0,0 +1,23 @@
+import * as cdk from 'aws-cdk-lib';
+import { Construct } from 'constructs';
+import * as s3 from 'aws-cdk-lib/aws-s3';
+// import * as sqs from 'aws-cdk-lib/aws-sqs';
+
+export class SiteIacStack extends cdk.Stack {
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    // The code that defines your stack goes here
+
+    new s3.Bucket(this, 'SampleBucket', {
+      bucketName: 'sample',
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+      autoDeleteObjects: true,
+    });
+
+    // example resource
+    // const queue = new sqs.Queue(this, 'SiteIacQueue', {
+    //   visibilityTimeout: cdk.Duration.seconds(300)
+    // });
+  }
+}
